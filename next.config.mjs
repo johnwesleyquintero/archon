e8d57828d053e106a7c7c82cadc4a228c0b5169a
John Wesley Quintero <johnwesleyquintero@gmail.com>
2025-08-02 09:02:07 +0800

    docs: remove outdated GitHub Actions README file
    The documentation was outdated and no longer reflected the current GitHub Actions configuration. The information has been moved to the project's main documentation.

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,14 @@ const nextConfig = {
         protocol: "https",
         hostname: "**.v0.dev",
       },
+      {
+        protocol: "https",
+        hostname: new URL(process.env.NEXT_PUBLIC_APP_URL || "").hostname,
+      },
+      {
+        protocol: "https",
+        hostname: new URL(process.env.VERCEL_URL || "").hostname,
+      },
     ],
   },
 };
@@ -22,33 +30,4 @@ import { withSentryConfig } from "@sentry/nextjs";
 
 // Make sure adding Sentry options is the last code to run before exporting, to ensure that
 // other plugins can't change the webpack config after Sentry has moved the sourcemaps.
-export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
-
-  // Suppresses source map uploading logs during build time.
-  silent: !process.env.CI,
-  org: process.env.SENTRY_ORG || "nebula-singularity",
-  project: process.env.SENTRY_PROJECT || "archon",
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: false,
-
-  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  // tunnelRoute: "/monitoring",
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: false,
-});
+export default nextConfig;
