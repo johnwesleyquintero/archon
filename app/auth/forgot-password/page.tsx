99e9cf73ad7d4d9e156fe769625cb0aecde4099a
John Wesley Quintero <johnwesleyquintero@gmail.com>
2025-08-03 00:56:11 +0800

    feat(app): update app/auth/forgot-password/page.tsx

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -27,9 +27,7 @@ export const metadata: Metadata = {
 
 export default function ForgotPasswordPage({
   searchParams,
-}: {
-  searchParams: any;
-}) {
+}: { searchParams: any }) {
   const forgotPassword = async (formData: FormData) => {
     "use server";
 
